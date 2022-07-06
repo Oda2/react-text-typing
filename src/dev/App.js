b7f027a76a6a260f50258caf7ffcae9ea05ef60d
Renato Oda <renato.oda@mercadolivre.com>
2022-07-06 00:19:43 -0300

    feat: Migration to styled-components

--- src/dev/App.js
+++ src/dev/App.js
@@ -3,7 +3,7 @@ import Text from '../lib';
 
 const App = () => (
   <div className="content">
-    <Text text="Example Text" showBlink={true} component="h1" />
+    <Text text="Example Text" showBlink={true} component="h1" colorText="red" colorTyping="#fff" />
   </div>
 );
 
